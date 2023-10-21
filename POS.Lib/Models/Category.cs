106fe33e682a2906216fe61fc18ca3b5f913e741
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-10-21 15:19:07 +0700

    reset

--- POS.Lib/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace POS.Lib.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string CategoryName { get; set; } = null;
-        public string Description { get; set; } = null;
-    }
-}
