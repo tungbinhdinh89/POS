129fd64beecf7a4d3ecd06ef14e253eab3410824
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-10-21 15:45:57 +0700

    add model product

---
+++ POS.Lib/Models/Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Lib.Models
+{
+    public class Models
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null;
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; } = null;
+        public double? UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public int Discontinued { get; set; }
+
+        public string CategoryName { get; set; } = null;
+        public string SupplierName { get; set; } = null;
+
+    }
+}
